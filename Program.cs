be64788b06fe4bd7ad4c3f7c99deb79d70f5742d
Abuu7707 <rahmatovv7707@gmai.com>
2025-08-09 16:58:04 +0300

    Task 1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int Even = 0;
+int Odd = 0;
+int Digit = 0;
+int Sum = 0;
+for (int i = a; i > 0; i /= 10)
+{
+    int t = i % 10;
+    if (t % 2 == 0) Even++;
+    if (t % 2 != 0) Odd++;
+    if (t > 0) Digit++;
+    if (t > 0) Sum += t;
+}
+System.Console.WriteLine($"Even:{Even}");
+System.Console.WriteLine($"Even:{Odd}");
+System.Console.WriteLine($"Even:{Digit}");
+System.Console.WriteLine($"Even:{Sum}");
